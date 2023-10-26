58e639f2c98e3ad355ef90b8e823b5c624214734
HugoMarinho-UA <146828912+HugoMarinho-UA@users.noreply.github.com>
2023-10-26 09:31:42 +0100

    Add files via upload

--- print_bits.c
+++ print_bits.c
@@ -3,12 +3,38 @@
 
 void binario (int i)
 {
-   if (i == 0)
-   printf("%d", i);
-   else{
-    binario(i / 2);
-    printf("%d", i % 2);
-   } 
+    if (i < 0)
+    {
+        printf("-");
+        i = abs(i);
+    }
+    int nonZeroPrinted = 0;  // A flag to track if non-zero digit is printed
+
+    for (int j = 31; j >= 0; j--)
+    {
+        int bit = (i >> j) & 1;  // Extract the j-th bit
+        if (bit == 1 || nonZeroPrinted)
+        {
+            printf("%d", bit);
+            nonZeroPrinted = 1;
+        }
+    }
+    if (!nonZeroPrinted)
+    {
+        printf("0");  // Print at least one zero for the value 0
+    }
+    /*
+    if (i == 0)
+    {
+        printf("0");
+    }
+        else
+        {
+            binario(i / 2);
+            printf("%d", i % 2);
+        } 
+    */    
+   
 }
 int main ()
 {
@@ -17,5 +43,7 @@ int main ()
     scanf("%d", &i);  
 
     binario(i);
+    printf("\n");
 
+    return 0;
 }
